ed612c975309a2451dab773c36545ce8a164a5a6
Tehreem Arooj <tehreem.arooj@tintash.com>
2023-07-03 15:38:49 +0500

    update code

--- decode-cryptomatte.py
+++ decode-cryptomatte.py
@@ -5,42 +5,44 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 mapping = {
-    "CryptoPreview": {"role": imagecat.data.Role.RGB, "selection": ["uCryptoMaterial.red", "uCryptoMaterial.green", "uCryptoMaterial.blue"]},
+    "CryptoPreview": {"role": imagecat.data.Role.RGB, "selection": ["R", "G", "B"]},
 }
 
 graph = graphcat.DynamicGraph()
 imagecat.add_task(graph, "/load", imagecat.operator.load,
-                  path="./Camera_14.exr")
+                  path="./Sample.exr")
 imagecat.add_task(graph, "/preview", imagecat.operator.remap, mapping=mapping)
 imagecat.add_links(graph, "/load", ("/preview", "image"))
 
 graphcat.notebook.display(graph)
 image = graph.output("/preview")
 
+imagecat.operator.save(graph, 'file_saver', [image,'./outputs','*'])
+
 image.layers["CryptoPreview"]
 
 graph.output("/load").metadata
 
-imagecat.add_task(graph, "/cryptomatte", imagecat.operator.cryptomatte.decoder,
-                  clown=False, mattes=["bunny_porcelain_mat"])
-imagecat.add_links(graph, "/load", ("/cryptomatte", "image"))
+# imagecat.add_task(graph, "/cryptomatte", imagecat.operator.cryptomatte.decoder,
+#                   clown=False, mattes=["bunny_porcelain_mat"])
+# imagecat.add_links(graph, "/load", ("/cryptomatte", "image"))
 
-graphcat.notebook.display(graph)
-image = graph.output("/cryptomatte")
+# graphcat.notebook.display(graph)
+# image = graph.output("/cryptomatte")
 
-image.layers["M"]
+# image.layers["M"]
 
-graph.set_task("/cryptomatte/mattes",
-               graphcat.constant(["flowerA_petal", "flowerB_petal"]))
-image = graph.output("/cryptomatte")
+# graph.set_task("/cryptomatte/mattes",
+#                graphcat.constant(["flowerA_petal", "flowerB_petal"]))
+# image = graph.output("/cryptomatte")
 
-image.layers["M"]
-
-
-graph.set_task("/cryptomatte/clown", graphcat.constant(True))
-graph.set_task("/cryptomatte/mattes", graphcat.constant(["bunny_porcelain_mat", "flowerA_petal",
-               "flowerB_petal", "flowerStem_mat", "grass_mat", "ground_mat", "smallLeaf_mat", "smallStalk_mat"]))
-image = graph.output("/cryptomatte")
+# image.layers["M"]
 
 
-image.layers["M"]
+# graph.set_task("/cryptomatte/clown", graphcat.constant(True))
+# graph.set_task("/cryptomatte/mattes", graphcat.constant(["bunny_porcelain_mat", "flowerA_petal",
+#                "flowerB_petal", "flowerStem_mat", "grass_mat", "ground_mat", "smallLeaf_mat", "smallStalk_mat"]))
+# image = graph.output("/cryptomatte")
+
+
+# image.layers["M"]
